7803a4eae562b35b7469514165623d5b8c92dea8
Michele Capri <michele.capri@synthesis3.it>
2022-06-23 02:49:22 +0200

    Small fix

--- src/controllers/Examinations.ts
+++ src/controllers/Examinations.ts
@@ -11,35 +11,51 @@ const Covid19Examinations: Examination[] = data;
 
 // getting all examinations
 const getAll = async (req: Request, res: Response) => {
-    // get examinations
-    const pageSize = req.query.pageSize as unknown as number;
-    const page = req.query.page as unknown as number;
+    try{
+        // get examinations
+        const pageSize = req.query.pageSize as unknown as number;
+        const page = req.query.page as unknown as number;
 
-    const examinations = getAllExaminations(Covid19Examinations, pageSize, page);
+        const examinations = getAllExaminations(Covid19Examinations, pageSize, page);
+        
+        return res.send(examinations);
+    }
+    catch (e) {
+        return res.status(500).json({ status: 500, message: "Internal server error" });
+    }
     
-    return res.send(examinations);
 };
 
 // getting all examinations by location id
 const getByLocationId = async (req: Request, res: Response) => {
-    // get the examination locationId from the req
-    const locationId: number = +req.params.locationId;
-    // get the examinations
-    const examinations = getExaminationsByLocationId(Covid19Examinations, locationId);
+    try{
+        // get the examination locationId from the req
+        const locationId: number = +req.params.locationId;
+        // get the examinations
+        const examinations = getExaminationsByLocationId(Covid19Examinations, locationId);
 
-    return res.send(examinations);
+        return res.send(examinations);
+    }
+    catch (e) {
+        return res.status(500).json({ status: 500, message: "Internal server error" });
+    }
 };
 
 // getting stats
 const getStats = async (req: Request, res: Response) => {
-    // get stats
+    try{
+        // get stats
 
-    const dateFrom = req.query.dateFrom as unknown as Date;
-    const dateTo = req.query.dateTo as unknown as Date;
+        const dateFrom = req.query.dateFrom as unknown as Date;
+        const dateTo = req.query.dateTo as unknown as Date;
 
-    let stats: Stat[] = getExaminationStats(Covid19Examinations, dateFrom, dateTo);
+        let stats: Stat[] = getExaminationStats(Covid19Examinations, dateFrom, dateTo);
 
-    return res.send(stats);
+        return res.send(stats);
+    }
+    catch (e) {
+        return res.status(500).json({ status: 500, message: "Internal server error" });
+    }
 };
 
 
--- src/managers/Examination.ts
+++ src/managers/Examination.ts
@@ -4,63 +4,78 @@ import { Stat } from "../models/Stat";
 module.exports = { getAllExaminations, getExaminationsByLocationId, getExaminationStats };
 
 function getAllExaminations(Covid19Examinations: Examination[], pageSize: number, page: number) {
-    let examinations;
+    try{
+        let examinations;
 
-    if (pageSize !== undefined && page !== undefined)
-        examinations = Covid19Examinations.slice((page - 1) * pageSize, page * pageSize);
-
-    else
-        examinations = Covid19Examinations;
-    return examinations;
+        if (pageSize !== undefined && page !== undefined)
+            examinations = Covid19Examinations.slice((page - 1) * pageSize, page * pageSize);
+    
+        else
+            examinations = Covid19Examinations;
+        return examinations;
+    }
+    catch (e) {
+        throw Error('Error getting all examinations')
+    }
 }
 
 function getExaminationsByLocationId(Covid19Examinations: Examination[], locationId: number){
-    const examinations = Covid19Examinations.filter((obj) => {
-        return obj.locationId === locationId;
-    });
-    return examinations;
+    try{
+        const examinations = Covid19Examinations.filter((obj) => {
+            return obj.locationId === locationId;
+        });
+        return examinations;
+    }
+    catch (e) {
+        throw Error('Error getting examinations by location id')
+    }
 }
 
 function getExaminationStats(Covid19Examinations: Examination[], dateFrom: Date, dateTo: Date) {
-    let examinations;
+    try{
+        let examinations;
 
-    if (dateFrom !== undefined && dateTo !== undefined)
-        examinations = Covid19Examinations.filter((item: Examination) => item.date >= dateFrom && item.date <= dateTo);
-
-    else
-        examinations = Covid19Examinations;
-
-    let stats: Stat[] = [];
-    examinations.forEach((item: Examination) => {
-        let locationId = item.locationId;
-        let pending = 0;
-        let negative = 0;
-        let positive = 0;
-
-        let stat = stats.find((obj) => {
-            return obj.locationId === locationId;
+        if (dateFrom !== undefined && dateTo !== undefined)
+            examinations = Covid19Examinations.filter((item: Examination) => item.date >= dateFrom && item.date <= dateTo);
+    
+        else
+            examinations = Covid19Examinations;
+    
+        let stats: Stat[] = [];
+        examinations.forEach((item: Examination) => {
+            let locationId = item.locationId;
+            let pending = 0;
+            let negative = 0;
+            let positive = 0;
+    
+            let stat = stats.find((obj) => {
+                return obj.locationId === locationId;
+            });
+    
+            if (!stat) {
+                stats.push({ locationId, pending, negative, positive });
+            }
+    
+            switch (item.result) {
+                case 'pending':
+                    pending = 1;
+                    break;
+                case 'negative':
+                    negative = 1;
+                    break;
+                case 'positive':
+                    positive = 1;
+                    break;
+            }
+    
+            stat = stats.find(location => location['locationId'] === locationId);
+            stat!.pending += pending;
+            stat!.negative += negative;
+            stat!.positive += positive;
         });
-
-        if (!stat) {
-            stats.push({ locationId, pending, negative, positive });
-        }
-
-        switch (item.result) {
-            case 'pending':
-                pending = 1;
-                break;
-            case 'negative':
-                negative = 1;
-                break;
-            case 'positive':
-                positive = 1;
-                break;
-        }
-
-        stat = stats.find(location => location['locationId'] === locationId);
-        stat!.pending += pending;
-        stat!.negative += negative;
-        stat!.positive += positive;
-    });
-    return stats;
+        return stats;
+    }
+    catch (e) {
+        throw Error('Error getting examinations stats')
+    }
 }
